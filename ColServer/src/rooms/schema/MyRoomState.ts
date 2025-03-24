09c9e67630653ece5250c996b808f6ef6c8842c7
milsimural <108474929+milsimural@users.noreply.github.com>
2025-03-24 17:06:29 +0300

    add server monorep

---
+++ ColServer/src/rooms/schema/MyRoomState.ts
@@ -0,0 +1,13 @@
+import { Schema, type, MapSchema } from "@colyseus/schema";
+
+export class Player extends Schema {
+  @type("number") x: number = 0;
+  @type("number") y: number = 0;
+  @type("number") z: number = 0;
+}
+
+export class MyRoomState extends Schema {
+
+  @type({ map: Player }) players = new MapSchema<Player>();
+
+}
