85a61ede0066ee4badd542548567189256907d82
milsimural <108474929+milsimural@users.noreply.github.com>
2025-05-06 17:42:27 +0300

    Update MyRoomState.ts

--- ColServer/src/rooms/schema/MyRoomState.ts
+++ ColServer/src/rooms/schema/MyRoomState.ts
@@ -6,8 +6,12 @@ export class Player extends Schema {
   @type("number") z: number = 0;
 }
 
+export class Base extends Schema {
+  @type("number") x: number = 0;
+  @type("number") y: number = 0;
+  @type("number") z: number = 0;
+}
+
 export class MyRoomState extends Schema {
-
   @type({ map: Player }) players = new MapSchema<Player>();
-
 }
